991e7b2d5aa8372a0089860b05eead934b30e873
Accounts <accounts@hewcorplaw.com>
2026-06-12 18:08:25 +0200

    Add STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,7 @@ DATE_INPUT_FORMATS = ['%d/%m/%Y', '%Y-%m-%d']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STASTATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
